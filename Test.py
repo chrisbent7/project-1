aa908bf5b7baa5af70aaa073c835f2fb4ed3b282
Christopher Bent <christopherbent@Christophers-MacBook-Pro-2.local>
2021-08-18 22:06:03 -0500

    project 1 complete

--- Test.py
+++ Test.py
@@ -9,3 +9,23 @@ y=5
 y=y+6
 
 print(y)
+
+a = "text.txt"
+print(a)
+
+b = open('text.txt', "r")
+print( b.read() )
+
+
+c = b.read()
+print(c)
+
+b = open('text.txt', 'r')
+
+new = open('text2.txt', 'w')
+
+for filewritethingy in b:
+ new.write(filewritethingy.replace('my name is christopher', 'my name is chris'))
+new.close()
+new = open('text2.txt', 'r')
+print(new.read())
